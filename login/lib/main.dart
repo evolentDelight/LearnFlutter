5367832579c3580c0bac157b9b96629713a26d4a
evolentDelight <evolentDelight@outlook.com>
2023-10-18 07:21:46 -0700

    Custom Authentication Code

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -61,4 +63,28 @@ class _LoginFormState extends State<LoginForm>{
       ],)
     );
   }
+}
+
+Future<bool> isLoginSuccessful(String login, String pass) async{
+  String token = "";//Retrieve token
+
+  try {
+    final userCredential =
+        await FirebaseAuth.instance.signInWithCustomToken(token);
+    print("Sign-in successful.");
+    return true;
+} on FirebaseAuthException catch (e) {
+    switch (e.code) {
+        case "invalid-custom-token":
+            print("The supplied token is not a Firebase custom auth token.");
+            break;
+        case "custom-token-mismatch":
+            print("The supplied token is for a different Firebase project.");
+            break;
+        default:
+            print("Unkown error.");
+    }
+    return false;
+}
+
 }
