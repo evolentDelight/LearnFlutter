c93c22559a86a8502fb6f044abac26e281a52b0f
evolentDelight <evolentDelight@outlook.com>
2023-10-18 08:11:55 -0700

    Configure added Realtime Database Plugin

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//Initialize Firebase and add plugins
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
 
 void main() {
   runApp(const MainApp());
